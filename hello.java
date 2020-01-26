466e2d35106b1ed3c6034ec9f749b222d5663119
timir <kazi.monirul699@gmail.com>
2020-01-27 00:31:02 +0600

    added a new method call display to hello.java

--- hello.java
+++ hello.java
@@ -2,4 +2,7 @@ public class Hello {
     public static void main(string[] args) {
         System.out.printIn("vs code is awesome...");
     }
+    public static void display(string name) {
+        system.out.printIn("Welcome" +name );
+    }
 }
