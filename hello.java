b12835f80af2590ca17e77829e34f6d11937dfe7
timir <kazi.monirul699@gmail.com>
2020-01-27 01:03:45 +0600

    change greeting method

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
 public class Hello {
     public static void main(string[] args) {
         System.out.printIn("vs code is awesome...");
+        greeting("git");
     }
     public static void display(string name) {
-        system.out.printIn("Welcome" +name );
+        system.out.printIn("bye" +name );
     }
 }
+
