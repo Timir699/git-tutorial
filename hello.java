6dc9e83b2afa21bc8c273502c641b994759234d0
timir <kazi.monirul699@gmail.com>
2020-01-27 01:07:36 +0600

    add a new method sqr

--- hello.java
+++ hello.java
@@ -5,4 +5,8 @@ public class Hello {
     public static void display(string name) {
         system.out.printIn("Welcome" +name );
     }
+
+    public static int sqr(int x) {
+        return x*x;
+    }
 }
