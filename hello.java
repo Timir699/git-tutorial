90ad5970e8d5876bfab963cce0c65a6766f421f6
timir <kazi.monirul699@gmail.com>
2020-01-27 11:28:41 +0600

    all method removed add msg method

--- hello.java
+++ hello.java
@@ -1,14 +1,9 @@
 public class Hello {
     public static void main(string[] args) {
-        System.out.printIn("vs code is awesome...");
-        greeting("git");
+       
     }
-    public static void display() {
-        system.out.printIn("Hello Hello" );
-    }
-
-    public static int sqr(int x) {
-        return x*x;
+    public static void message (){
+        system.out.println("hooray!! Now we are able to contribute on open source")
     }
 
 }
